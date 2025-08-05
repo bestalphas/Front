0502d84889899ce4be581e184f1bff9d60585bb5
bestalphas <bestalpha@naver.com>
2025-08-05 11:55:07 +0900

    modified:   src/api/todoApi.js 	modified:   src/components/todo/ModifyComponent.js

--- src/components/todo/ModifyComponent.js
+++ src/components/todo/ModifyComponent.js
@@ -1,4 +1,7 @@
 import { useEffect,useState } from "react";
+import { getOne,putOne,deleteOne} from "../../api/todoApi";
+import ResultModal from "../common/ResultModal";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState = {
     tno:0,
@@ -12,17 +15,104 @@ const ModifyComponent = ({tno}) =>{
     
     const[todo,setTodo] = useState({...initState})
     
+    const[result,setResult] = useState(null)
+
+    const { moveToList, moveToRead } = useCustomMove()
     useEffect(()=>{
-        
+        getOne(tno).then(data =>{
+            setTodo(data)
+        })
     },[tno])
     
+    const handleClickModify = () =>{
+        putOne(todo).then(data=>{
+            console.log("수정결과" + data)
+            setResult('Modified')
+        })
+    }
+
+    const handleClickDelete=()=>{
+        deleteOne(tno).then(data=>{
+            console.log("삭제 결과: " + data)
+            setResult('삭제성공')
+        })
+    }
+
+    const closeModal = () =>{
+        if(result ==='삭제성공'){
+            moveToList()
+        }else{
+            moveToRead(tno)
+        }
+    }
+
+
+    const handleChangeTodo =(e) =>{
+        todo[e.target.name]=e.target.value
+
+        setTodo({...todo})
+    }
+
+    const handleChangeTodoComplete = (e) =>{
+        const value = e.target.value
+        todo.complete = (value ==='Y')
+        setTodo({...todo})
+    }
+    
     return(
-        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+        
+        <div className="border-2 border-sky-200 mt-2 m-2 p-4">
+            
+            {result ? <ResultModal title={'처리결과'} content={result} callbackFn={closeModal}></ResultModal> : <></>}
+            
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">번호</div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md bg-gray-100">{todo.tno}</div>
+                    </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">작성자</div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md bg-gray-100">{todo.writer}</div>
+                    </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">제목</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md" name="title" type={'text'} value={todo.title} onChange={handleChangeTodo}></input>
+                    </div>
+            </div>
+
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">작성일시</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md" name="dueDate" type={'date'} value={todo.dueDate} onChange={handleChangeTodo}></input>
+                    </div>
+            </div>        
+                    
+
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">완료여부</div>
+                    <select name="status" className="border-solid border-2 rounded m-1 p-2" onChange={handleChangeTodoComplete} value={todo.complete? 'Y':'N'}>
+                        <option value='Y'>완료</option>
+                        <option value='N'>수정중</option>
+                    </select>
+                </div>
+            </div>
+                
+                    
+        
         <div className="flex justify-end p-4">
-        <button type="button" className="rounded p-4 w-36 bg-red-500 text-xl text-white">삭제</button>
-        <button type="button" className="rounded p-4 w-36 bg-blue-500 text-xl text-white">수정</button>
+        <button type="button" className="rounded p-4 w-36 bg-red-500 text-xl text-white" onClick={handleClickDelete}>삭제</button>
+        <button type="button" className="rounded p-4 w-36 bg-blue-500 text-xl text-white" onClick={handleClickModify}>수정</button>
         </div>
+        
         </div>
+        
     )
 }
 
