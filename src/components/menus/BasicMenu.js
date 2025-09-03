a9bd37de84446958f661857d8c43230c4f1a7406
bestalphas <bestalpha@naver.com>
2025-09-03 16:30:28 +0900

    modified:   package-lock.json 	modified:   package.json 	new file:   src/components/member/LoginComponent.js 	modified:   src/components/menus/BasicMenu.js 	modified:   src/index.js 	new file:   src/pages/member/LoginPage.js 	new file:   src/router/memberRouter.js 	modified:   src/router/root.js 	new file:   src/slices/loginSlice.js 	new file:   src/store.js

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
 
 const BasicMenu = () =>{
+    
+    const loginState = useSelector(state => state.loginSlice)
+    
     return(
         <nav id ='navbar' className="flex bg-blue-300">
             <div className="w-4/5 bg-gray-500">
@@ -11,19 +15,32 @@ const BasicMenu = () =>{
                     <li className="pr-6 text-2xl">
                         <Link to={'/about'}>About</Link>
                     </li>
+                    
+                    {loginState.email ? <>
+                    
                     <li className="pr-6 text-2xl">
                         <Link to={'/todo/'}>Todo</Link>
                     </li>
                     <li className="pr-6 text-2xl">
-                        <Link to={'/products'}>Products</Link>
+                        <Link to={'/products/'}>Products</Link>
                     </li>
+                    </>
+                    :
+                    <></>
+                    }
                 </ul>
             </div>
 
             <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
+                
+                {!loginState.email ?
+
                 <div className="text-white text-sm m-1 rounded">
-                    Login
+                    <Link to={'/member/login'}>로그인</Link>
                 </div>
+                :
+                <></>
+                }
             </div>
         </nav>
     )
